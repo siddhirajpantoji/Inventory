a85d75ddd00fb1e5da0f9c2c773cf3518b4eca6f
Siddhiraj Pantoji <siddhirajpantoji@gmail.com>
2018-02-04 15:17:22 +0530

    Changes For Constantant

--- inventory/src/main/java/com/inventory/constants/MessageConstants.java
+++ inventory/src/main/java/com/inventory/constants/MessageConstants.java
@@ -13,8 +13,6 @@ public class MessageConstants {
 	public static final String SOMETHING_WENT_WRONG = "Something went wrong ";
 	public static final String METHOD_NOT_SUPPORTED = "Method not Supported ";
 	public static final String INVALID_REQUEST = "Invalid Request ";
-
 	public static final String DUPLICATE_ENTRY = "This record already exists  ";
-
 	public static final String CATEGORY_ALREADY_EXISTS = "This category already exists ";
 }
